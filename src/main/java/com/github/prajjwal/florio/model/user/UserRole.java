9e961339dc26f16b1e789aa012c1e3d446b02b84
prajjwal <prajjwalpachauri725@gmail.com>
2025-08-28 14:38:00 +0530

    Created roles for user

---
+++ src/main/java/com/github/prajjwal/florio/model/user/UserRole.java
@@ -0,0 +1,13 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri
+Date: 28-08-2025
+Time: 2:34 pm
+File: UserRole.java */
+package com.github.prajjwal.florio.model.user;
+
+public enum UserRole {
+    ADMIN,
+    USER,
+    SERVICE_PROVIDER
+}
