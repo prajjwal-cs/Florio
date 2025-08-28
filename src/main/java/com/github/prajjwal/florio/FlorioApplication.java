c2a2a9f425f06638b9f9dc7d0634efb8f62fc656
prajjwal <prajjwalpachauri725@gmail.com>
2025-08-28 11:46:07 +0530

    Initial commit

---
+++ src/main/java/com/github/prajjwal/florio/FlorioApplication.java
@@ -0,0 +1,13 @@
+package com.github.prajjwal.florio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlorioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/prajjwal/florio/FlorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.prajjwal.florio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
