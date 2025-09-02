c70c8518bb2bd24b1ebfcd6f7515de1ae4fad21f
prajjwal <prajjwalpachauri725@gmail.com>
2025-09-02 14:09:44 +0530

    Created JwtAuthenticationFilter class

---
+++ src/main/java/com/github/prajjwal/florio/config/JwtAuthenticationFilter.java
@@ -0,0 +1,10 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri
+Date: 02-09-2025
+Time: 2:09 pm
+File: JwtAuthenticationFilter.java */
+package com.github.prajjwal.florio.config;
+
+public class JwtAuthenticationFilter {
+}
