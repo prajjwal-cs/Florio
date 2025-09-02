866fe8367557a6ee09e7f3612f9c6c8cde3b1bbb
prajjwal <prajjwalpachauri725@gmail.com>
2025-09-02 16:16:59 +0530

    implemented jwt authentication entry point class

--- src/main/java/com/github/prajjwal/florio/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/github/prajjwal/florio/config/JwtAuthenticationEntryPoint.java
@@ -6,30 +6,36 @@ Time: 6:35pm
 File: CustomAuthenticationEntryPoint.java */
 package com.github.prajjwal.florio.config;
 
-import jakarta.servlet.FilterChain;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
-public class JwtAuthenticationEntryPoint extends OncePerRequestFilter implements AuthenticationEntryPoint, Serializable {
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-            throws IOException, ServletException {
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-    }
+        Map<String, Object> body = new HashMap<>();
+        body.put("success", false);
+        body.put("message", "unauthorized: Authentication token required");
+        body.put("error", authException.getMessage());
+        body.put("path", request.getServletPath());
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
     }
 }
