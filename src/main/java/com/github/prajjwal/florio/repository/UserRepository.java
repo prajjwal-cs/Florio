00820dc8a509c357a1a132a203d79cc5e8c103d9
prajjwal <prajjwalpachauri725@gmail.com>
2025-09-02 13:56:56 +0530

    implemented user repository interface

--- src/main/java/com/github/prajjwal/florio/repository/UserRepository.java
+++ src/main/java/com/github/prajjwal/florio/repository/UserRepository.java
@@ -1,9 +1,22 @@
 package com.github.prajjwal.florio.repository;
 
 import com.github.prajjwal.florio.model.user.User;
-import org.springframework.data.repository.Repository;
+import com.github.prajjwal.florio.model.user.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends Repository<User, UUID> {
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<User> findByRole(UserRole role);
+
+    @Query("SELECT u FROM User u WHERE u.role = 'SERVICE_PARTNER' AND UPPER(u.specialization) = UPPER(:specialization)")
+    Optional<User> findServicePartnerBySpecialization(@Param(value = "specialization") String specialization);
 }
