92e49b77f10602e2034c572101da6d0d3b261bad
prajjwal <prajjwalpachauri725@gmail.com>
2026-02-26 11:30:39 +0530

    Implemented security config

--- src/main/java/com/github/prajjwal/florio/config/SecurityConfig.java
+++ src/main/java/com/github/prajjwal/florio/config/SecurityConfig.java
@@ -6,11 +6,15 @@ Time: 6:19pm
 File: SecurityConfig.java */
 package com.github.prajjwal.florio.config;
 
-import jakarta.servlet.Filter;
+import com.github.prajjwal.florio.util.CustomUserDetailsService;
+import com.github.prajjwal.florio.util.OAuth2LoginFailureHandler;
+import com.github.prajjwal.florio.util.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,12 +29,23 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private final JwtAuthenticationEntryPoint authenticationEntryPoint;
     private final JwtAuthenticationFilter authenticationFilter;
+    private final CustomUserDetailsService userDetailsService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
     @Bean
@@ -42,19 +57,33 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/public/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**", "/oauth2/**",
+                                "/login/oauth2/**", "/error").permitAll()
                         .requestMatchers("api/v1/admin/**").hasRole("ADMIN")
-                        .requestMatchers("api/v1/service-partner/**").hasRole("SERVICE_PARTNER")
-                        .requestMatchers("api/v1/customer/**").hasRole("CUSTOMER")
+                        .requestMatchers("api/v1/job-worker/**").hasRole("JOB_WORKER")
+                        .requestMatchers("api/v1/users/**").hasRole("USER")
                         .anyRequest().authenticated()
+                )
+                .oauth2Login(oAuth2 -> oAuth2
+                        .authorizationEndpoint(endpoint ->
+                                endpoint.baseUri("/oauth2/authorize")
+                        )
+                        .redirectionEndpoint(endpoint ->
+                                endpoint.baseUri("/login/oauth2/code/*")
+                        )
+                        .successHandler(oAuth2LoginSuccessHandler)
+                        .failureHandler(oAuth2LoginFailureHandler)
+
                 )
                 .exceptionHandling(exception ->
                         exception.authenticationEntryPoint(authenticationEntryPoint))
                 .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(daoAuthenticationProvider());
 
         http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
